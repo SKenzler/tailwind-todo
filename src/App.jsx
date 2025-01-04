ab5738109e46f32a695ad720ac3578969dcee938
Shane Kenzler <shanekenzler@gmail.com>
2025-01-04 10:23:44 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TodoUI from "./components/TodoUI";
+import TodoCard from "./components/ToDoCard";
+function App() {
+  return (
+    <>
+      <TodoUI />
+      <TodoCard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TodoUI.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoUI = () => {
+  return <div>TodoUI</div>;
+};
+
+export default TodoUI;
